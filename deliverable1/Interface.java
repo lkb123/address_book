96bf9a58bfbb516f0803c81d5083126aa032ed8e
Louie Kert <louiebasay@gmail.com>
2013-11-30 00:12:35 +0800

    Revert "Revert "november 28-29, 2013""
    This reverts commit a3ca885186d24cb92a6e1abaec953782bbb0af8b.

---
+++ deliverable1/Interface.java
@@ -0,0 +1,153 @@
+package deliverable1;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.KeyStroke;
+import javax.swing.ListSelectionModel;
+
+public class Interface extends JPanel {
+	
+	static JFrame mainFrame = new JFrame("Untitled");
+	static JPanel listPanel = new JPanel();
+	static JPanel buttonPanel = new JPanel();
+	
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new Interface();
+			}
+		});
+	}
+	
+	public Interface() {
+		//Dimension defSize = Toolkit.getDefaultToolkit().getScreenSize();
+		
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame.setMinimumSize(new Dimension(700, 500));
+		mainFrame.setResizable(false);
+		
+		listPanel.setPreferredSize(new Dimension(500, 300));
+		//listPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		buttonPanel.setPreferredSize(new Dimension(100, 100));
+		//buttonPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		
+		createMenu();
+		createList(setEntries());
+		addButtons();
+		
+		//add(listPanel, BorderLayout.CENTER);
+		//add(buttonPanel, FlowLayout.LEFT);
+		add(listPanel, BorderLayout.CENTER);
+		add(buttonPanel, BorderLayout.SOUTH);
+		
+		mainFrame.getContentPane().add(this);
+		//mainFrame.getContentPane().add(buttonPanel);
+		mainFrame.setVisible(true);
+	}
+	
+	public static void createMenu() {
+		JMenuBar mbar = new JMenuBar();
+		JMenu menu;
+		JMenuItem item;
+		
+		//file menu
+		menu = new JMenu("File");
+		menu.setMnemonic(KeyEvent.VK_F);
+		
+		//new
+		item = new JMenuItem("New", KeyEvent.VK_N);
+		item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK));
+		menu.add(item);
+		//open
+		item = new JMenuItem("Open", KeyEvent.VK_O);
+		item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.CTRL_MASK));
+		menu.add(item);
+		//Save
+		item = new JMenuItem("Save", KeyEvent.VK_S);
+		item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK));
+		menu.add(item);
+		//Save As
+		item = new JMenuItem("Save As", KeyEvent.VK_A);
+		item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, ActionEvent.CTRL_MASK));
+		menu.add(item);
+		//-----------------------------
+		menu.addSeparator();
+		item = new JMenuItem("Print", KeyEvent.VK_P);
+		item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_P, ActionEvent.CTRL_MASK));
+		menu.add(item);
+		//-----------------------------		
+		menu.addSeparator();
+		item = new JMenuItem("Quit", KeyEvent.VK_Q);
+		item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, ActionEvent.CTRL_MASK));
+		item.addActionListener(new QuitListener());
+		menu.add(item);
+		
+		mbar.add(menu);
+		
+		//sort menu
+		menu = new JMenu("Sort");
+		menu.setMnemonic(KeyEvent.VK_O);
+		mbar.add(menu);
+		
+		//search menu
+		menu = new JMenu("Search");
+		menu.setMnemonic(KeyEvent.VK_E);
+		mbar.add(menu);
+		
+		mainFrame.setJMenuBar(mbar);
+	}
+	
+	public static void createList(ArrayList<String> entry) {
+		DefaultListModel<String> entries = new DefaultListModel<String> ();
+		//entries.addElement(new Entry("Louie Kert", "Basay"));
+		//entries.addElement(new Entry("Feliadulfo", "Basay"));
+		//entries.addElement(new Entry("Vilma", "Basay"));
+		//entries.addElement(new Entry("Wabi Dahran", "Basay"));
+		//entries.addElement(new Entry("Khristian", "Basay"));
+		//entries.addElement(new Entry("Michelle Ann", "Bation"));
+		for(String s : entry) {
+			entries.addElement(s);
+		}
+		
+		JList<String> list = new JList<String>(entries);
+		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		list.setLayoutOrientation(JList.VERTICAL);
+		list.setPreferredSize(new Dimension(500, 290));
+		list.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		listPanel.add(list);
+	}
+	
+	public static void addButtons() {
+		JButton add = new JButton("Add");
+		JButton edit = new JButton("Edit");
+		JButton delete = new JButton("Delete");
+		
+		buttonPanel.add(add);
+		buttonPanel.add(edit);
+		buttonPanel.add(delete);
+		//panel.setLayout(new FlowLayout());
+	}
+	
+	public static ArrayList<String> setEntries() {
+		ArrayList<String> list = new ArrayList<String> ();
+		list.add("Louie");
+		list.add("Michelle");
+		return list;
+	}
+}
---
+++ deliverable1/QuitListener.java
@@ -0,0 +1,26 @@
+package deliverable1;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JOptionPane;
+
+/**
+ * @author Louie Kert Basay
+ * @date
+ * 
+ */
+public class QuitListener implements ActionListener {
+
+	/* (non-Javadoc)
+	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	 */
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		String s = "Are you sure you want to quit?";
+		int choice = JOptionPane.showConfirmDialog(null, s, s, JOptionPane.YES_NO_OPTION);
+		if(choice == JOptionPane.YES_OPTION)
+			System.exit(0);
+	}
+
+}
